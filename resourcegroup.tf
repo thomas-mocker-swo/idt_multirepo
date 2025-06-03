17f953ac3b2f8aef409924e5236c2a053b58c16a
Thomas.Mocker <thomas.mocker@oebb.at>
2025-06-03 11:18:00 +0200

    typo

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "identity" {
   tags     = var.tags
 }
 
-resource "azurerm_resource_group" "identity" {
+resource "azurerm_resource_group" "tmocker" {
   name     = "rg-tmocker-${var.location_short}-${var.environment}"
   location = var.location
   tags     = var.tags
